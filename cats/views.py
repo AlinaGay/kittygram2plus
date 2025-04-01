5a8f6664d7977b1e5e6eb057285311eb5cae3a58
AlinaGay <gayfullina.a@gmail.com>
2025-04-01 13:33:58 +0200

    Add filtration

--- cats/views.py
+++ cats/views.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
+from rest_framework import filters, viewsets
 from rest_framework.throttling import AnonRateThrottle
 
 from .models import Achievement, Cat, User
@@ -13,11 +13,15 @@ class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
     permission_classes = (OwnerOrReadOnly,)
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter)
     throttle_classes = (AnonRateThrottle, WorkingHoursRateThrottle)
     throttle_scope = 'low_request'
     pagination_class = CatsPagination
     filterset_fields = ('color', 'birth_year')
+    search_fields = ('^name',)
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
